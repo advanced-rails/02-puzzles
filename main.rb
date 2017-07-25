5b783cf9c4d769aac7f6c4a37b63c5cea983ad59
Chyld Medford <chyld.medford@gmail.com>
2017-07-25 18:03:44 +0000

    initials

--- main.rb
+++ main.rb
@@ -2,4 +2,8 @@ module Puzzle
     def self.add(a, b)
         a + b
     end
+    
+    def self.initials(str)
+        str.upcase.split.map{|w| w[0]}.join
+    end
 end
--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
 require_relative './main'
 
 p Puzzle.add(3, 4)
+p Puzzle.initials('bOb FrAnK sMith')
