902c291547a129bdb148dfd3463ac410029b0280
Chyld Medford <chyld.medford@gmail.com>
2017-07-25 21:58:58 +0000

    cartesian

--- test.rb
+++ test.rb
@@ -1,8 +1,18 @@
 require_relative './main'
 require_relative './dog'
+require_relative './point'
+require_relative './cartesian'
 
 p Puzzle.add(3, 4)
 p Puzzle.initials('bOb FrAnK sMith')
 p Puzzle.expand(345)
 
 p ['fid', 'lassssy', 'felix'].map{|s| Dog.new(s, s.size).to_s}
+
+c = Cartesian.new(Point.new(1,2), Point.new(3,5))
+p c.distance
+p c.line
+
+c = Cartesian.new(Point.new(1,-20), Point.new(3,5))
+p c.distance
+p c.line
